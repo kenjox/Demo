63155c9c7b6c60d1a64af245fa0cdfe84780a1d2
Otis <kennedy.otis@gmail.com>
2014-06-05 09:51:43 +0200

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Kennedy Otis on 2014-06-05.
+//  Copyright (c) 2014 kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Kennedy Otis on 2014-06-05.
+//  Copyright (c) 2014 kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
